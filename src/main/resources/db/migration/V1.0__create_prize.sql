1834c11138ae5d743581b946837f082af21cd9f5
francisco <ortega.franciscojavier@gmail.com>
2021-08-31 00:16:32 +0200

    Refactor of database crreation and update to do in two steps, create table and insert data

--- src/main/resources/db/migration/V1.0__create_prize.sql
+++ src/main/resources/db/migration/V1.0__create_prize.sql
@@ -10,9 +10,3 @@ CREATE TABLE IF NOT EXISTS PRICES (
   CURRENCY varchar(255),
   PRIMARY KEY (ID)
 );
-
-insert into PRICES values (1,1,PARSEDATETIME('2020-06-14-00.00.00','yyyy-MM-dd-HH.mm.ss'),PARSEDATETIME('2020-12-31-23.59.59','yyyy-MM-dd-HH.mm.ss'), 1, 35455, 0, 35.50, 'EUR');
-insert into PRICES values (2,1,PARSEDATETIME('2020-06-14-15.00.00','yyyy-MM-dd-HH.mm.ss'),PARSEDATETIME('2020-06-14-18.30.00','yyyy-MM-dd-HH.mm.ss'), 2, 35455, 1, 25.45, 'EUR');
-insert into PRICES values (3,1,PARSEDATETIME('2020-06-15-00.00.00','yyyy-MM-dd-HH.mm.ss'),PARSEDATETIME('2020-06-15-11.00.00','yyyy-MM-dd-HH.mm.ss'), 3, 35455, 1, 30.50, 'EUR');
-insert into PRICES values (4,1,PARSEDATETIME('2020-06-15-16.00.00','yyyy-MM-dd-HH.mm.ss'),PARSEDATETIME('2020-12-31-23.59.59','yyyy-MM-dd-HH.mm.ss'), 4, 35455, 1, 38.95, 'EUR');
-
