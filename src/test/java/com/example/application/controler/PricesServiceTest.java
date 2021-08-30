2e121e3a9dfb044985fb54f6437fbf5bfebed118
francisco <ortega.franciscojavier@gmail.com>
2021-08-30 23:52:57 +0200

    Refactor to remove unncessary

--- src/test/java/com/example/application/controler/PricesServiceTest.java
+++ src/test/java/com/example/application/controler/PricesServiceTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class PricesServiceTest {
 
